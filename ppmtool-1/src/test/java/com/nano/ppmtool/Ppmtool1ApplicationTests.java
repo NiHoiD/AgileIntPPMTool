4745cbf4e62c6309bb679ac1919c2694e2253667
NiHoiD <jamaleddinenassar@gmail.com>
2021-07-18 12:40:14 +0400

    First Commit

---
+++ ppmtool-1/src/test/java/com/nano/ppmtool/Ppmtool1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nano.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppmtool1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
